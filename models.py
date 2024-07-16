055d1261158ee8155b9a0a42ceb2b1cbcf8e913a
breadbread1984 <breadbread1984@163.com>
2024-07-16 08:05:32 +0000

    update

--- models.py
+++ models.py
@@ -24,6 +24,7 @@ class Mamba(LLM):
     input_ids = input_ids.to(self.model.device)
     out = self.model.generate(input_ids, do_sample = False, temperature = 0.8, top_p = 0.8, max_new_tokens = 2048)
     out = out[len(input_ids):]
+    out = self.tokenizer.batch_decode(out)[0]
     return out
   @property
   def _llm_type(self):
