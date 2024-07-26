740ad4b1f7bd58e9e1d0acd803f068859fded76e
breadbread1984 <breadbread1984@163.com>
2024-07-26 16:26:15 +0800

    update

--- social_mining/create_web.py
+++ social_mining/create_web.py
@@ -62,7 +62,7 @@ def main(unused_argv):
     idx = sheet.iloc[i]['序号']
     title = sheet.iloc[i]['Article Title']
     pdf_path = join(FLAGS.paper_dir, f"{idx}-{title}.pdf")
-    if not exists(path_path): continue
+    if not exists(pdf_path): continue
     output = subprocess.check_output('anystyle -f bib find %s' % pdf_path, shell = True, text = True)
     with open('tmp.bib','w') as f:
       f.write(output)
