a55a6dfa0cf9e6f13cb5ca8a9181fd31a6b26ab8
breadbread1984 <breadbread1984@163.com>
2024-07-30 15:14:56 +0800

    update

---
+++ paper_filtering/prompts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from langchain_core.prompts.prompt import PromptTemplate
+
+def summarize_template(tokenizer, additional_instructions = None, accumulated = False):
+  messages = [
+    {'role': 'system', 'content': 'Rewrite this text in summarized form.' + ("" if additional_instructions is None else f"\n\n{additional_instructions}")},
+    {'role': 'user', 'content': "{chunk}" if accumulated == False else "Previous summaries:\n\n{accumulated_summaries_string}\n\n{chunk}"}
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['chunk'] if accumulated == False else ['chunk', 'accumulated_summaries_string'])
+  return template
