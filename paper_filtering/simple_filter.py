1dca2dbdd21bd5de0b4450bf460d2b6e0a862838
breadbread1984 <breadbread1984@163.com>
2024-07-30 16:24:54 +0800

    update

---
+++ paper_filtering/simple_filter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+from os import walk
+from os.path import join, exists, splitext
+import json
+from tqdm import tqdm
+from langchain.document_loaders import UnstructuredPDFLoader
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('input_dir', default = None, help = 'path to directory containing papers')
+  flags.DEFINE_string('output_json', default = 'output.json', help = 'path to output json')
+
+def main(unused_argv):
+  unrelevant_files = list()
+  for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
+    for f in files:
+      stem, ext = splitext(f)
+      if ext != '.pdf': continue
+      loader = UnstructuredPDFLoader(join(root, f), mode = 'single')
+      text = ''.join([doc.page_content for doc in loader.load()])
+      relevant = text.find('sulfide') >= 0 and text.file('electrolyte') >= 0
+      unrelevant_files.append(join(root, f))
+  with open(FLAGS.output_json, 'w') as f:
+    f.write(json.dumps(unrelevant_files))
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
+
