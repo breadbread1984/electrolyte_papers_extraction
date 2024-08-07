13c072d7cf9362dbde7edf2dc74b771a9019f913
breadbread1984 <breadbread1984@163.com>
2024-08-07 10:01:02 +0800

    update

--- NER/run_cypher.sh
+++ NER/run_cypher.sh
@@ -2,7 +2,7 @@
 
 HOST="bolt://localhost:7687"
 USERNAME="neo4j"
-PASSWORD="neo4j"
+PASSWORD="19841124"
 DATABASE="neo4j"
 
 cat output.cypher | cypher-shell -a "$HOST" -u "$USERNAME" -p "$PASSWORD" -d "$DATABASE"
