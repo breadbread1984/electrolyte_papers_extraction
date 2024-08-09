2258f7d9c93009310c26c69fac5ccb86707fa38b
breadbread1984 <breadbread1984@163.com>
2024-08-09 02:44:17 +0000

    update

--- NER/preprocess.py
+++ NER/preprocess.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 from uuid import uuid4
 from os import mkdir, walk
 from os.path import exists, isdir, join, splitext
+import pandas as pd
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
@@ -15,14 +16,20 @@ def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to input directory')
   flags.DEFINE_string('output_dir', default = 'output', help = 'path to output directory')
   flags.DEFINE_integer('length', default = 500, help = 'sample sentence length')
+  flags.DEFINE_string('list', default = None, help = 'path to xls of papers')
 
 def main(unused_argv):
   if exists(FLAGS.output_dir): rmtree(FLAGS.output_dir)
   mkdir(FLAGS.output_dir)
+  paper_list = None if FLAGS.list is None else pd.ExcelFile(FLAGS.list)
   text_splitter = RecursiveCharacterTextSplitter(chunk_size = FLAGS.length, chunk_overlap = 0)
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
       stem, ext = splitext(f)
+      idx = int(stem.split('-')[0])
+      assert paper_list.iloc[idx + 1]['序号'] == idx
+      doi = paper_list.iloc[idx + 1]['DOI'].replace('/','_')
+      output_name = (str(uuid4()) if (FLAGS.list is None or doi is None or doi == '') else doi) + '.txt'
       if ext.lower() in ['.htm', '.html']:
         loader = UnstructuredHTMLLoader(join(root, f))
       elif ext.lower() == '.txt':
