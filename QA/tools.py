302e21bd992a893064a73d824a49d198f27f23bf
breadbread1984 <breadbread1984@163.com>
2024-09-06 15:34:58 +0800

    update

--- QA/tools.py
+++ QA/tools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from typing import Optional, Type, List, Dict, Union, Any
-from neo4j import GraphDatabase
+from neo4j import GraphDatabase, BoltDriver
 from transformers import AutoTokenizer, PreTrainedTokenizerFast, CodeLlamaTokenizer
 from langchain.llms.base import LLM
 from langchain.pydantic_v1 import BaseModel, Field
@@ -10,17 +10,21 @@ from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, Callback
 
 def load_knowledge_graph(host = "bolt://localhost:7687", username = "neo4j", password = "neo4j", database = "neo4j", tokenizer = None, llm = None):
   class Input(BaseModel):
-    query: str = Field(description = "question about attributes of electrolyte")
+    query: str = Field(description = "electrolyte in chemical formula format")
   class Config(BaseModel):
     class Config:
       arbitrary_types_allowed = True
-    neo4j: GraphDatabase
+    neo4j: BoltDriver
+    db: str
     tokenizer: Union[PreTrainedTokenizerFast, CodeLlamaTokenizer]
     llm: LLM
   class Tool(BaseTool):
     name = "electrolyte property query tool"
-    description = "you can find ionic conductivity, energy density, current density and crystal structure of an electrolyte with this tool"
+    description = "you can query ionic conductivity, energy density, current density and crystal structure of an electrolyte with this tool"
     args_schema: Type[BaseModel] = Input
     return_direct: boll = True
     config: Config
-    def _run(self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str
+    def _run(self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
+      records, summary, keys = config.neo4j.execute_query("match (a: SULFIDE_ELECTROLYTE)-[r]->(b) where a.name = $f return b as attribute", f = query, database_ = config.db)
+      attributes = [record['attribute'] for record in records]
+
